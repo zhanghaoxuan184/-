a47fa796dc85463417c142ae211db20644dff6e7
Raymond <46810845+zhanghaoxuan184@users.noreply.github.com>
2020-11-26 15:14:28 +0800

    the UI of software

--- app/src/main/java/com/example/wechat_ui/MemoryRead.java
+++ app/src/main/java/com/example/wechat_ui/MemoryRead.java
@@ -19,14 +19,16 @@ public class MemoryRead extends AppCompatActivity {
         setContentView(R.layout.page_03);
 
         imageButton = (ImageButton) findViewById(R.id.read_button_to_edit);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-
+        imageButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                //跳转页面
                 Intent intent = new Intent(MemoryRead.this, MemoryEdit.class);
                 startActivity(intent);
 
-
+                return false;
             }
+
         });
 
 
